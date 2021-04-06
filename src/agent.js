c40e95780a1c40d9dcdf239cab41d1ce1ffb9e2e
kmai <kam@penneo.com>
2021-04-06 12:06:54 -0300

    Changing API endpoint to POC's

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,8 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+//const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'https://api.poc.datariah.com/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
